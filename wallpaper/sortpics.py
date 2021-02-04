d85d1eae6fff9d23abd7d7a475f8ce0f231e72b2
Nate Fuller <natef@gurutechnologies.net>
2021-02-03 18:59:01 -0700

    improved sortpics

--- wallpaper/sortpics.py
+++ wallpaper/sortpics.py
@@ -2,11 +2,12 @@
 import subprocess
 import re
 
-imageRE = re.compile(r"\.(jpg|JPG|png|PNG)")
+imageRE = re.compile(r"(?<!Landscape)(?<!Portrait).*\.(jpg|JPG|png|PNG)")
 sizeRE = re.compile(r"(\d+)x(\d+)")
+folderRE = re.compile(r"(/?[^/]*/)")
 
 #get list of pictures
-list_of_files = subprocess.run(["ls"], capture_output=True, encoding="utf-8").stdout.split('\n')
+list_of_files = subprocess.run("find ~/.wallpaper -follow -maxdepth 2 -type f ", shell=True, capture_output=True, encoding="utf-8").stdout.split('\n')
 list_of_images = [f for f in list_of_files if imageRE.search(f)]
 
 # create folders if they don't exist
@@ -21,9 +22,15 @@ for pic in list_of_images:
         width = int(m.group(1))
         height = int(m.group(2))
 
+        # capture subfolder info
+        folderinfo = folderRE.findall(pic)
+        subfolder = ""
+        if folderinfo:
+            subfolder = folderinfo[-1]
+
         # determine if it's portrait or landscape;
         # put it in the right folder
         if width > height:
-            subprocess.run(["mv", pic, "Landscape/"])
+            subprocess.run(["mv", pic, "Landscape/"+subfolder])
         else:
-            subprocess.run(["mv", pic, "Portrait/"])
+            subprocess.run(["mv", pic, "Portrait/"+subfolder])
